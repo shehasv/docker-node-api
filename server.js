006c076b4f73c336f599581737a3207cc6a64e06
MOHAMED SHEHAS V <mohamedshehas.v2@mindtree.com>
2021-03-07 00:25:17 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (req,res) =>{
+    res.json([{
+        name:'Shehas',
+        email:'shehasktpm@gail.com'
+    }])
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running in port ${PORT} `)
+})
